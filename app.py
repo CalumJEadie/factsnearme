07b0f5fc6509aea86db4c6425e0a3be45b88f089
Calum J. Eadie <calum@calumjeadie.com>
2013-08-23 13:49:50 +0100

    Added dummy end point

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import web
+import json
         
 urls = (
-    '/(.*)', 'hello'
+    '/articles/(.+)/(.+)', 'Article'
 )
-app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello, ' + name + '!'
+class Article:
+    """
+    e.g. GET /articles/51.525603/-0.087558
+    """
+
+    def GET(self, lat, lon):
+
+        articles = [
+            {
+                "title": "Stonehenge",
+                "first_sentence": "Stonehenge is a prehistoric monument in Wiltshire, England, about 2 miles (3.2 km) west of Amesbury and 8 miles (13 km) north of Salisbury.",
+                "image": "http://upload.wikimedia.org/wikipedia/commons/3/3c/Stonehenge2007_07_30.jpg"
+            }
+        ]
+        web.header('Content-Type', 'application/json')
+        return json.dumps(articles)
 
 if __name__ == "__main__":
+    app = web.application(urls, globals())
     app.run()
